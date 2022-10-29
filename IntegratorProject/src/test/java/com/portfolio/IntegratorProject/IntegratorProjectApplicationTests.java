b59f8541ee9f8659c5bd61e0f6c8d814f1c9563c
Ana <101430818+AnnLuz@users.noreply.github.com>
2022-10-28 21:47:40 -0300

    Proyecto Spring boot
    Creación de mi primer proyecto con Spring boot

---
+++ IntegratorProject/src/test/java/com/portfolio/IntegratorProject/IntegratorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.IntegratorProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
